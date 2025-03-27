a652fa937b1784e5a3a986a371be39d20c790679
Thomas Silva <thomas@allto.digital>
2025-03-27 11:24:57 -0300

    Atualização da versão do Mongoose e ajustes na configuração de conexão com o MongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 module.exports = mongoose.connect("mongodb://localhost/todo", {
-  useMongoClient: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
